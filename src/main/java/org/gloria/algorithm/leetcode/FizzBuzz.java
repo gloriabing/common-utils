efc1c22bca2069ab0fdbc695643bb16a3d259289
gloriabing <zhangbing1990@126.com>
2017-01-10 10:40:35 +0800

    添加题目说明

--- src/main/java/org/gloria/algorithm/leetcode/FizzBuzz.java
+++ src/main/java/org/gloria/algorithm/leetcode/FizzBuzz.java
@@ -9,6 +9,34 @@ import java.util.List;
  * Create on 2017/1/5 10:40.
  *
  * @author : gloria.
+ * 
+ * Write a program that outputs the string representation of numbers from 1 to n.
+ *
+ * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+ *
+ * Example:
+ *
+ * n = 15,
+ *
+ * Return:
+ * [
+ * "1",
+ * "2",
+ * "Fizz",
+ * "4",
+ * "Buzz",
+ * "Fizz",
+ * "7",
+ * "8",
+ * "Fizz",
+ * "Buzz",
+ * "11",
+ * "Fizz",
+ * "13",
+ * "14",
+ * "FizzBuzz"
+ * ]
+ *
  */
 public class FizzBuzz {
 
